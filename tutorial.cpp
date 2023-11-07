abe6d57a30d8785777e3f83445eb357e8915cef8
Anton <anton.shatalin@gmail.com>
2023-11-07 04:57:50 +0000

    what

---
+++ tutorial.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int m,b;
+
+int main()
+{   
+    cout << "input m" <<endl ;
+    cin >> m ;
+    cout << "input b" ;
+    cin >> b ;
+    int sum = m * b;
+    cout << sum << endl;
+    return 0;
+}
